13a83040fdbb96b67d12066e0bb563f650dc922d
Romaine Allen <romallen1@gmail.com>
2021-08-07 15:46:28 +0900

    Implement form and validate for password match & empty fields

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
 (function () {
   let h1= document.getElementById('title')
-  h1.innerText = "Hello World"
+  h1.className = "header1";
+  h1.innerText = "New Account "
+
+
+  //document.getElementById("userInput").submit();
+
+  let newAcct = {};
+ let createAcct = [];
+//create div for error messages
+ let message = document.createElement("div");
+ message.id ="message";
+ message.innerHTML = "";
+ document.getElementById("messageArea").append(message);
+
+async function userSubmit(){
+
+  message.innerHTML = ""
+  
+
+  newAcct["nameInput"] = await document.getElementById("name").value;
+  newAcct["email"] = await document.getElementById("email").value;
+  newAcct["pass1"] = await document.getElementById("pass1").value;
+  newAcct["pass2"] = await document.getElementById("pass2").value;
+
+//Checks that all field have been inputted
+  for(let key in newAcct){
+    if(newAcct[key] === ""){
+      message.innerText = "The " + key + " field is empty!!"
+      document.getElementById(key).reset()
+    }
+  }
+
+  // checks if the passwords match
+  if(newAcct["pass1"] !== newAcct["pass2"]){
+    
+    message.innerHTML = "The password fields do not match!!";
+
+    document.getElementById("pass1").value = ""; 
+    document.getElementById("pass2").value = "";
+  
+  }
+
+  
+  console.log(newAcct)
+}
+  window.addEventListener("submit", userSubmit);
+
+
+
+
+    // const nameInput = document.createElement('div')
+    // let nameText = document.createElement('name');
+    // nameText.className = 'textLable';
+    // nameText.innerText = "Name";
+    // nameInput.append(nameText);
+
+  
+
 })()
